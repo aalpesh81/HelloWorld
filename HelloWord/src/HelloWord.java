cb40cf305e1b383c08f93e8a985af76f081f04bc
aalpesh81 <alpesh_savani@yahoo.co.in>
2016-12-07 19:32:08 +0000

    Commit changes from IntelliJ to GitHub

---
+++ HelloWord/src/HelloWord.java
@@ -0,0 +1,12 @@
+/**
+ * Created by alpesh_savani on 07/10/2016.
+ */
+public class HelloWord {
+
+    public static void main(String args[]) {
+        System.out.println("\"Hello World\"");
+        System.out.println("\"This is my very Fisrt JAVA Projcet.\"");
+        System.out.println("\"Eat Java, Drink Java, Think Java, Sleep Java\"");
+        System.out.println("\"This is Direct Change and Commit from IntelliJ to GitHub!!\"");
+    }
+}
